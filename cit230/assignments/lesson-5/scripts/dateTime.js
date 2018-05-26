67dcf649217e37a1c11d118398ed912999d9fef9
jljennings <38636661+jljennings@users.noreply.github.com>
2018-05-26 01:13:43 -0600

    lesson-5

---
+++ cit230/assignments/lesson-5/scripts/dateTime.js
@@ -0,0 +1,6 @@
+var test = new Date();
+var month = test.getMonth();
+month = (month + 1);
+var day = test.getDate();
+var year = test.getFullYear();
+document.write(" ", month, "/", day, "/", year, " ");
